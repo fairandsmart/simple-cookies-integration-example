b031f4312d43134ee6c01d5921daba50aa9bc4ef
Nicolas Rueff <nicolas@enere.fr>
2021-06-16 10:31:27 +0200

    switched to new static / dynamic integration style

--- README.md
+++ README.md
@@ -14,8 +14,10 @@ docker).
 | parameter  | environment variable name  | config file key  | 
 |---|---|---|
 | api server url | API_URL | api_url |
-| organisation id | ORGANISATION_ID | organisation_id |
-| model id | MODEL_ID | model_id |
+| the cookie config id when in dynamic mode (= unsigned and alway take the last version) | DYNAMIC_COOKIE_CONFIG_ID | dynamic_cookie_config_id |
+| the cookie config id when in static mode (= signed but take a specific version version)| STATIC_COOKIE_CONTENT_ID | static_cookie_content_id |
+| the cookie integrity checksum when in static mode | STATIC_COOKIE_CONTENT_INTEGRITY | static_cookie_content_integrity |
+
 
 Configuration file is loaded from CONFIG_FILE_PATH (default : "config.ini"). 
  
@@ -23,3 +25,7 @@ Configuration file is loaded from CONFIG_FILE_PATH (default : "config.ini").
 Under docker, using a specific config file "my-config.ini" :
 
 `docker run --mount type=bind,source=$PWD/my-config.ini,target=/var/www/html/my-config.ini -e CONFIG_FILE_PATH="my-config.ini" simple-cookies-integration-example`
+
+## using static version
+
+Append "static" as argument : http://localhost/?static instead of http://localhost
--- index.php
+++ index.php
@@ -3,25 +3,20 @@ function get_conf()
 {
     $config = parse_ini_file(key_exists("CONFIG_FILE_PATH", $_ENV) ? $_ENV["CONFIG_FILE_PATH"] : "config.ini");
     $config["api_url"] = key_exists("API_URL", $_ENV) ? $_ENV["API_URL"] : $config["api_url"];
-    $config["organisation_id"] = key_exists("ORGANISATION_ID", $_ENV) ? $_ENV["ORGANISATION_ID"] : $config["organisation_id"];
-    $config["model_id"] = key_exists("MODEL_ID", $_ENV) ? $_ENV["MODEL_ID"] : $config["model_id"];
-    $config["tac_url"] = key_exists("TAC_URL", $_ENV) ? $_ENV["TAC_URL"] : $config["tac_url"];
+    $config["dynamic_cookie_config_id"] = key_exists("DYNAMIC_COOKIE_CONFIG_ID", $_ENV) ? $_ENV["DYNAMIC_COOKIE_CONFIG_ID"] : $config["dynamic_cookie_config_id"];
+    $config["static_cookie_content_id"] = key_exists("STATIC_COOKIE_CONTENT_ID", $_ENV) ? $_ENV["STATIC_COOKIE_CONTENT_ID"] : $config["static_cookie_content_id"];
+    $config["static_cookie_content_integrity"] = key_exists("STATIC_COOKIE_CONTENT_INTEGRITY", $_ENV) ? $_ENV["STATIC_COOKIE_CONTENT_INTEGRITY"] : $config["static_cookie_content_integrity"];
     return $config;
 }
 
-function get_cookie_conf_url()
+function get_dynamic_cookie_script()
 {
-    return get_conf()["api_url"] . "/ext/cookies/config/" . get_conf()["organisation_id"] . "/" . get_conf()["model_id"] . "/script";
+    return '<script src="' . get_conf()["api_url"] . '/ext/cookies/clients/' . get_conf()["dynamic_cookie_config_id"] . '"></script>';
 }
 
-function get_cookie_style_url()
+function get_static_cookie_script()
 {
-    return get_conf()["api_url"] . "/ext/cookies/config/" . get_conf()["organisation_id"] . "/" . get_conf()["model_id"] . "/style";
-}
-
-function get_cookie_code_url()
-{
-     return get_conf()["tac_url"];
+    return '<script src="' . get_conf()["api_url"] . '/ext/cookies/content/' . get_conf()["static_cookie_content_id"] . '" integrity="' . get_conf()["static_cookie_content_integrity"] . '"></script>';;
 }
 
 ?>
@@ -38,23 +33,23 @@ function get_cookie_code_url()
             .content { width: 100%; display: flex; flex-direction: row; justify-content: space-evenly; }
             .button-wrapper { margin-top: 100px; }
         </style>
-        <!-- Balises copiées-collées -->
-        <script type="text/javascript" src="<?php echo get_cookie_code_url();?>"></script>
-        <script type="text/javascript" src="<?php echo get_cookie_conf_url();?>"></script>
-        <link rel="stylesheet" type="text/css" href="<?php echo get_cookie_style_url();?>">
-        <!-- Ouverture manuelle du panneau de configuration des cookies -->
+        <?php echo key_exists("static", $_GET) ? get_static_cookie_script() : get_dynamic_cookie_script();?>
         <script> function openPanel() { document.location.hash = "#cookie_demo"; } </script>
     </head>
     <body>
+
     <h2>Simple Cookies Integration Example</h2>
     <!-- Balises spécifiques des services -->
         <div class="content">
             <div class="left">
                 <div class="share">
                     <div class="fb-like" data-layout="standard" data-action="like" data-share="TRUE"></div>
-                    <span class="tacTwitter"></span>
-                    <a href="https://twitter.com/share" class="twitter-share-button" data-size="LARGE" data-via="@FairandSmart" data-count="VERTICAL" data-dnt="true"></a>
+                    <br/>
                 </div>
+                <span class="tacTwitter"></span>
+                    <br/>
+                    <a href="https://twitter.com/share" class="twitter-share-button" data-size="LARGE" data-via="@FairandSmart" data-count="VERTICAL" data-dnt="true"></a>
+                <br/>
                 <div class="twitterembed-canvas" tweetid="1336270558577745921" theme="light" cards="SHOW" conversation="NONE" data-align="LEFT"></div>
             </div>
             <div class="right">
